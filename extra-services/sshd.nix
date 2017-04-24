4a1ef10f2eface7f01646f4f9e9ec4cdeb457e2d
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2017-04-24 09:30:33 +0300

    Add missing sshd.nix

---
+++ extra-services/sshd.nix
@@ -0,0 +1,22 @@
+{ lib, config, ... }:
+with lib;
+{
+
+  options.localConfiguration.extraServices.sshd = mkOption {
+    type = types.bool;
+    default = false;
+  };
+
+  config = let
+    cfg = config.localConfiguration.extraServices;
+  in mkIf cfg.sshd {
+
+    services.openssh = {
+      enable = true;
+      permitRootLogin = "no";
+      ports = [6662];
+      passwordAuthentication = true;
+    };
+  };
+
+}
