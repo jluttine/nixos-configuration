98d7d9c6f634162be66fc7685bff9d263c0e703d
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2017-11-22 20:03:47 +0200

    Improve syncthing configuration, add reverse proxy

--- extra-services/syncthing.nix
+++ extra-services/syncthing.nix
@@ -2,12 +2,47 @@
 with lib;
 {
 
-  options.localConfiguration.extraServices.syncthing = mkOption {
-    type = types.attrs;
-    default = {};
+  options.localConfiguration.extraServices.syncthing = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    user = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+    };
+    domain = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+    };
   };
 
-  config.services.syncthing =
-    config.localConfiguration.extraServices.syncthing;
+  config.services = let
+    cfg = config.localConfiguration.extraServices.syncthing;
+    userConfig = if cfg.user == null then {} else {
+      user = cfg.user;
+      group = cfg.user;
+      dataDir = "/home/${cfg.user}/.syncthing/config";
+    };
+  in mkIf cfg.enable {
+
+    syncthing = {
+      enable = true;
+    } // userConfig;
+
+    nginx = if cfg.domain == null then {} else {
+      enable = true;
+      virtualHosts."${cfg.domain}" = {
+        forceSSL = true;
+        enableACME = true;
+        locations = {
+          "/" = {
+            proxyPass = "http://localhost:8384/";
+          };
+        };
+      };
+    };
+
+  };
 
 }
