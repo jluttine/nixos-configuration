88831948ef1a9ce1763d7027e49e7ef57fa0d28e
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2018-02-24 12:05:00 +0200

    Update Tiny Tiny RSS server configuration

--- extra-services/rss.nix
+++ extra-services/rss.nix
@@ -16,68 +16,22 @@ with lib;
     cfg = config.localConfiguration.extraServices.rss;
   in mkIf cfg.enable {
 
-    # services.nginx = {
-    #   enable = true;
-    #   virtualHosts."${cfg.domain}" = {
-    #     root = "/var/lib/selfoss";
-    #     locations."/" = {
-    #       extraConfig = ''
-    #         fastcgi_split_path_info ^(.+\.php)(/.+)$;
-    #         fastcgi_pass unix:/var/run/phpfpm/selfoss_pool.sock;
-    #         fastcgi_index index.php;
-    #       '';
-    #     };
-    #     # root = "${cfg.root}";
-
-    #     # locations."/" = {
-    #     #   index = "index.php";
-    #     # };
-
-    #     # locations."~ \.php$" = {
-    #     #   extraConfig = ''
-    #     #     fastcgi_split_path_info ^(.+\.php)(/.+)$;
-    #     #     fastcgi_pass unix:${phpfpmSocketName};
-    #     #     fastcgi_index index.php;
-    #     #   '';
-    #     # };
-    #   };
-    # };
-    # services.selfoss = {
-    #   enable = true;
-    # };
-
-
-    # users.extraUsers = [
-    #   {
-    #     name = "tt_rss";
-    #     uid = 422;
-    #   }
-    # ];
-    # users.extraUsers = optionalAttrs (cfg.user == "nginx") (singleton
-    #   { name = "nginx";
-    #     group = cfg.group;
-    #     uid = config.ids.uids.nginx;
-    #   });
-    # services.nginx.enable = true;
-    # services.mysql = {
-    #   enable = true;
-    #   package = pkgs.mariadb;
-    #   ensureDatabases = [ "tt_rss" ];
-    #   ensureUsers = [
-    #     {
-    #       name = "tt_rss";
-    #       ensurePermissions = {
-    #         "tt_rss.*" = "ALL PRIVILEGES";
-    #       };
-    #     }
-    #   ];
-    # };
+    # Set up Tiny Tiny RSS server
     services.tt-rss = {
       enable = true;
       virtualHost = cfg.domain;
-      selfUrlPath = "http://${cfg.domain}/";
+      selfUrlPath = "https://${cfg.domain}/";
       database = {
         type = "mysql";
+      };
+    };
+
+    # Use SSL
+    services.nginx = {
+      enable = true;
+      virtualHosts."${cfg.domain}" = {
+        forceSSL = cfg.ssl;
+        enableACME = cfg.ssl;
       };
     };
 
