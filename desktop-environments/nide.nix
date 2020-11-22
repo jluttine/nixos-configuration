e3f323a3f9500a169508a617143c5c024ead96b1
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2020-11-22 15:30:52 +0200

    Refactor NiDE into a separate package
    See: https://github.com/jluttine/NiDE

--- desktop-environments/nide.nix
+++ desktop-environments/nide.nix
@@ -2,10 +2,22 @@
 
 let
   cfg = config.localConfiguration;
+  nide = builtins.fetchTarball "https://github.com/jluttine/NiDE/archive/master.tar.gz";
 in
 {
 
+  imports = [
+    "${nide}/nix/configuration.nix"
+  ];
+
   config = lib.mkIf (cfg.desktopEnvironment == "nide") {
+
+    # Do not cache when fetching tarballs without sha256. Then, NiDE tarball
+    # will always be fetched, it isn't cached for 1h.
+    nix.extraOptions = ''
+      tarball-ttl = 0
+    '';
+
     # Open ports for KDE Connect
     networking.firewall.allowedTCPPorts = [
                           1714 1715 1716 1717 1718 1719
@@ -24,173 +36,17 @@ in
       1760 1761 1762 1763 1764
     ];
 
-    # Use none (i.e., xterm as the desktop manager)
-    services.xserver.desktopManager.xterm.enable = true;
-    services.xserver.displayManager.defaultSession = "xterm";
-
-    # Add DejaVu font and same fonts as Plasma
-    fonts.fonts = with pkgs; [
-      jetbrains-mono
-      dejavu_fonts
-      noto-fonts
-      hack-font
-      (nerdfonts.override {fonts=["Iosevka"];})
-    ];
-
-    # In order to set the default font for Simple Terminal, one has to patch
-    # this line: https://git.suckless.org/st/file/config.def.h.html#l8
-    #
-    # Or, alternatively, wrap st so that it is always called with -f flag. But
-    # then one cannot call st with another font flag.. Wait, yes they can! The
-    # last -f flag is used so one can override the previous options. Nice.
-
-    # These are the same fonts as Plasma uses
-    fonts.fontconfig.defaultFonts = {
-      monospace = [ "JetBrains Mono" ]; #"Hack" "Noto Mono" ];
-      sansSerif = [ "Noto Sans" ];
-      serif = [ "Noto Serif" ];
-    };
-
-    # environment.variables.SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh";
-
-    nixpkgs.overlays = [
-      (
-        self: super: {
-          polybar = super.polybar.override {
-            i3Support = true;
-            #mpdSupport = true;
-            pulseSupport = true;
-            #githubSupport = true;
-            nlSupport = false;
-            iwSupport = true;
-            wirelesstools = pkgs.wirelesstools;
-          };
-        }
-      )
-      (
-        # See: https://github.com/yshui/picom/issues/306
-        #
-        # Should be fixed in more recent versions than 7.5.
-        self: super: {
-          picom = (
-            super.picom.overrideAttrs (oldAttrs: {
-              src = pkgs.fetchFromGitHub {
-                owner = "yshui";
-                repo = "picom";
-                # See #306
-                # rev = "17831a7be3ecf02b874738009fbb2e244de6bd67";
-                # sha256 = "12c43xzvwbvn8bfmymd1798c7ayc7jhzl09h4628zv5krsk8crb3";
-                # See #381
-                rev = "d1f4969fc1efed4af5bd06ba86fdd11624e60a63";
-                sha256 = "1wxx63271bxrcnhylxcb2y770j0ibdzp8v16hhljdc1mp7723jbw";
-              };
-            })
-          );
-          #).override { debug = true; };
-        }
-      )
-      (
-        # KBDD doesn't work if it is started without a window in focus. It'll
-        # not detect EWMH support and fallback to some generic mode that doesn't
-        # remember the layout of the windows.. This patch just forces it to
-        # believe there is always EWMH support whether it can detect it or not.
-        # A better solution could be that KBDD would have a global state
-        # keyboard layout which is used when no window is focused.
-        #
-        # TODO: Add global (no window in focus) keyboard layout state.
-        self: super: {
-          kbdd = super.kbdd.overrideAttrs (oldAttrs: {
-            patches = [ ./kbdd.patch ];
-          });
-        }
-      )
-    ];
-    # Keybindings for adjusting monitor brightness. Adjusting brightness with
-    # actkbd+light is nice because it's independent of X and works in ttys.
-    services.actkbd = {
-      enable = true;
-      bindings = let
-        light = "${pkgs.light}/bin/light";
-        step = "10";
-      in [
-        {
-          keys = [ 224 ];
-          events = [ "key" ];
-          # Use minimum brightness 0.1 so the display won't go totally black.
-          command = "${light} -N 0.1 && ${light} -U ${step}";
-        }
-        {
-          keys = [ 225 ];
-          events = [ "key" ];
-          command = "${light} -A ${step}";
-        }
-      ];
-    };
-
-    # Enable users to modify brightness. NOTE: User must belong to video group!
-    # (These modify udev rules, so if you just enabled this, probably best to
-    # reboot.)
-    programs.light.enable = true;
-    hardware.acpilight.enable = true;
-
     environment.systemPackages = with pkgs; [
 
-      # NiDE requirements
-      i3
-      (rofi.override { plugins = [ rofi-file-browser ]; } )
-      # (rofi.override { configDir = "/tmp/rofi/"; })
-      #(rofi.override { configDir = "/home/jluttine/rofi-tmp/"; })
-      polybar
-      dunst
-      libnotify
-      dex
-      st
-      kbdd
-      gnome3.networkmanagerapplet
-      xss-lock
-      vlock
-      xsecurelock
-      xidlehook
-      picom
-      hsetroot  # picom-supported replacement for xsetroot
-
-
-      #
-      # Other minimal stuff
-      #
-
-      sxiv
-      mupdf
-      trash-cli
-      vimb
-      surf
-      qutebrowser
-      luakit
+      # File manager
       dolphin
-      arandr
-
-      # Password manager for KDE
-      # kdeFrameworks.kwallet
-      # kdeApplications.kwalletmanager
-      # kwalletcli
-
-      # Allow automatic unlocking of kwallet if the same password. This seems to
-      # work without installing kwallet-pam.
-      #kwallet-pam
-
-      # ssh-add prompts a user for a passphrase using KDE. Not sure if it is used
-      # by anything? ssh-add just asks passphrase on the console.
-      #ksshaskpass
+      kdeApplications.dolphin-plugins
 
       # Archives (e.g., tar.gz and zip)
       ark
 
       # GPG manager for KDE
       kgpg
-      # This is needed for graphical dialogs used to enter GPG passphrases
-      # pinentry_qt5
-
-      # kdeplasma-addons
 
       # Screenshots
       kdeApplications.spectacle
@@ -201,14 +57,8 @@ in
       # Text editor
       kate
 
-      # Torrenting
-      #ktorrent
-
       # Connect desktop and phone
       kdeconnect
-
-      # Drop-down terminal
-      yakuake
 
       # Printing and scanning
       kdeApplications.print-manager
@@ -222,11 +72,10 @@ in
       chromium
 
       # Email
-      #kmail
       thunderbird
 
       # Office suit
-      #libreoffice
+      libreoffice
 
       # Photo/image editor
       gwenview
@@ -237,44 +86,6 @@ in
 
       # Media player
       vlc
-
-      # KDE apps
-      # kdeFrameworks.kconfig
-      # kdeFrameworks.kconfigwidgets
-      # konsole
-      dolphin
-      kdeApplications.dolphin-plugins
-
-      # Perhaps not needed, but might be nice for managing..
-      gnome3.seahorse
-      # kdeApplications.kwalletmanager
-
-      # Not sure if needed here in systemPackages..?
-      # kdeFrameworks.kwallet
-      # libsForQt5.kwallet
-      # kwallet-pam
-
-      # In order to user Kwallet to store the passphrases, set the following in
-      # ~/.gnupg/gpg-agent.conf:
-      #
-      #   pinentry-program /run/current-system/sw/bin/pinentry-kwallet
-      #
-      # Then, gpg-agent will use Kwallet to ask the passphrases.
-      #
-      # However, I tried to use Kwallet but it didn't store the passphrase but
-      # "yes" string instead. Some bug somewhere? Well, it's also possible to
-      # use GNOME Keyring to store the passphrases by setting:
-      #
-      #   pinentry-program /run/current-system/sw/bin/pinentry-gnome3
-      #
-      # in ~/.gnupg/gpg-agent.conf. This seems to store the passphrases
-      # correctly.
-      gnupg
-      pinentry_gnome # contains pinentry-gnome3
-      pinentry-qt
-      pinentry
-      keychain
-      # kwalletcli     # contains pinentry-kwallet
 
       # For GNOME Keyring to work for git https protocol, use git-credential-libsecret:
       #
