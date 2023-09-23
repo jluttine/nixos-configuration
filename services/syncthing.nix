f304ef50e96d5233bab27f439958fb09d9b557c0
M3r11 <>
2023-09-23 14:37:00 +0300

    Update syncthing config

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -16,7 +16,7 @@
   config = {
     services.syncthing = {
       openDefaultPorts = lib.mkDefault true;
-      extraOptions = {
+      settings = {
         gui = let
           secrets = import ./syncthing-secrets.nix;
         in {
@@ -26,141 +26,141 @@
           user = secrets.user;
           password = secrets.password;
         };
-      };
-      # NOTE: Device and folder IDs are defined in syncthing-ids.nix
-      folders = let
-        isHost = n: (lib.toLower n) == (lib.toLower config.networking.hostName);
-        # Add default configs. Note that folderConfig can override the defaults
-        # because folderConfig is on the LHS of //
-        addDefaults = lib.mapAttrs (folderName: folderConfig:
-          {
-            path = "${config.services.syncthing.dataDir}/${folderName}";
-            enable = builtins.any isHost folderConfig.devices;
-            versioning = lib.mkDefault config.services.syncthing.defaultVersioning;
-          } // folderConfig
-        );
-        # All folders.
-        #
-        # TODO: Encrypt all folders on nipsu?
-        allFolders = {
-          Jaakko = {
-            devices = [
-              "Leevi"
-              "Nipsu"
-            ];
-          };
-          Meri = {
-            devices = [
-              "Martta"
-              "Nipsu"
-            ];
-          };
-          Yhteinen = {
-            devices = [
-              "Leevi"
-              "Martta"
-              "Nipsu"
-            ];
-          };
-          Liikkuva = {
-            devices = [
-              "Leevi"
-              "Martta"
-              "Nipsu"
-              "Taskuloinen"
-              "MerinOnePlus"
-            ];
-          };
-          Kamera-Jaakko = {
-            devices = [
-              "Leevi"
-              "Nipsu"
-              "Taskuloinen"
-            ];
-          };
-          Kamera-Meri = {
-            devices = [
-              "Martta"
-              "MerinOnePlus"
-              "Nipsu"
-            ];
-          };
-          Kalenteri-Jaakko = {
-            devices = [
-              "Nipsu"
-              "Taskuloinen"
-            ];
-          };
-          Kalenteri-Yhteinen = {
-            devices = [
-              "Nipsu"
-              "Taskuloinen"
-              "MerinOnePlus"
-            ];
-          };
-          Kontaktit-Jaakko = {
-            devices = [
-              "Nipsu"
-              "Taskuloinen"
-            ];
-          };
-          Orgzly-Jaakko = {
-            devices = [
-              "Leevi"
-              "Nipsu"
-              "Taskuloinen"
-            ];
-          };
-          Orgzly-Meri = {
-            devices = [
-              "Martta"
-              "MerinOnePlus"
-              "Nipsu"
-            ];
-          };
-          Orgzly-Yhteinen = {
-            devices = [
-              "Leevi"
-              "Martta"
-              "MerinOnePlus"
-              "Nipsu"
-              "Taskuloinen"
-            ];
-          };
-          Puhelut-Jaakko = {
-            devices = [
-              "Leevi"
-              "Nipsu"
-              "Taskuloinen"
-            ];
-          };
-          Instagram-Meri = {
-            devices = [
-              "Leevi"
-              "Martta"
-              "MerinOnePlus"
-              "Nipsu"
-              "Taskuloinen"
-            ];
-          };
-          Lompakot = {
-            devices = [
-              "Leevi"
-              "Martta"
-              "Nipsu"
-              "Taskuloinen"
-            ];
-            # TODO: Encrypt on mobiles and nipsu
-            #
-            # Never delete
-            versioning = lib.mkDefault {
-              type = "simple";
-              params.cleanoutDays = "0";
-              params.keep = "9999999";
+        # NOTE: Device and folder IDs are defined in syncthing-ids.nix
+        folders = let
+          isHost = n: (lib.toLower n) == (lib.toLower config.networking.hostName);
+          # Add default configs. Note that folderConfig can override the defaults
+          # because folderConfig is on the LHS of //
+          addDefaults = lib.mapAttrs (folderName: folderConfig:
+            {
+              path = "${config.services.syncthing.dataDir}/${folderName}";
+              enable = builtins.any isHost folderConfig.devices;
+              versioning = lib.mkDefault config.services.syncthing.defaultVersioning;
+            } // folderConfig
+          );
+          # All folders.
+          #
+          # TODO: Encrypt all folders on nipsu?
+          allFolders = {
+            Jaakko = {
+              devices = [
+                "Leevi"
+                "Nipsu"
+              ];
+            };
+            Meri = {
+              devices = [
+                "Martta"
+                "Nipsu"
+              ];
+            };
+            Yhteinen = {
+              devices = [
+                "Leevi"
+                "Martta"
+                "Nipsu"
+              ];
+            };
+            Liikkuva = {
+              devices = [
+                "Leevi"
+                "Martta"
+                "Nipsu"
+                "Taskuloinen"
+                "MerinOnePlus"
+              ];
+            };
+            Kamera-Jaakko = {
+              devices = [
+                "Leevi"
+                "Nipsu"
+                "Taskuloinen"
+              ];
+            };
+            Kamera-Meri = {
+              devices = [
+                "Martta"
+                "MerinOnePlus"
+                "Nipsu"
+              ];
+            };
+            Kalenteri-Jaakko = {
+              devices = [
+                "Nipsu"
+                "Taskuloinen"
+              ];
+            };
+            Kalenteri-Yhteinen = {
+              devices = [
+                "Nipsu"
+                "Taskuloinen"
+                "MerinOnePlus"
+              ];
+            };
+            Kontaktit-Jaakko = {
+              devices = [
+                "Nipsu"
+                "Taskuloinen"
+              ];
+            };
+            Orgzly-Jaakko = {
+              devices = [
+                "Leevi"
+                "Nipsu"
+                "Taskuloinen"
+              ];
+            };
+            Orgzly-Meri = {
+              devices = [
+                "Martta"
+                "MerinOnePlus"
+                "Nipsu"
+              ];
+            };
+            Orgzly-Yhteinen = {
+              devices = [
+                "Leevi"
+                "Martta"
+                "MerinOnePlus"
+                "Nipsu"
+                "Taskuloinen"
+              ];
+            };
+            Puhelut-Jaakko = {
+              devices = [
+                "Leevi"
+                "Nipsu"
+                "Taskuloinen"
+              ];
+            };
+            Instagram-Meri = {
+              devices = [
+                "Leevi"
+                "Martta"
+                "MerinOnePlus"
+                "Nipsu"
+                "Taskuloinen"
+              ];
+            };
+            Lompakot = {
+              devices = [
+                "Leevi"
+                "Martta"
+                "Nipsu"
+                "Taskuloinen"
+              ];
+              # TODO: Encrypt on mobiles and nipsu
+              #
+              # Never delete
+              versioning = lib.mkDefault {
+                type = "simple";
+                params.cleanoutDays = "0";
+                params.keep = "9999999";
+              };
             };
           };
-        };
-      in addDefaults allFolders;
+        in addDefaults allFolders;
+      };
     };
   };
 }
