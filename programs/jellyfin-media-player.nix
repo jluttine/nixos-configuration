90096d46fec94a2bd367fda45708e628a23fdf6a
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2026-01-23 22:08:34 +0200

    Remove jellyfin-media-player patching

--- programs/jellyfin-media-player.nix
+++ programs/jellyfin-media-player.nix
@@ -10,31 +10,6 @@
 
   config = {
     nixpkgs.overlays = [
-      (
-        self: super: {
-          jellyfin-media-player = super.jellyfin-media-player.overrideAttrs (
-            old: {
-              buildInputs = old.buildInputs ++ [ pkgs.makeWrapper ];
-              postInstall = let
-                envvars = lib.escapeShellArgs (
-                  builtins.concatLists (
-                    lib.mapAttrsToList (
-                      envvar: value: [ "--set" envvar value ]
-                    ) (
-                      # See:
-                      # https://github.com/jellyfin/jellyfin-media-player/issues/231#issuecomment-1293408450
-                      { QT_XCB_GL_INTEGRATION = "xcb_egl"; } //
-                      config.nixpkgs.jellyfinMediaPlayerEnv
-                    )
-                  )
-                );
-              in (old.postInstall or "") + ''
-                wrapProgram "$out/bin/jellyfin-desktop" ${envvars}
-              '';
-            }
-          );
-        }
-      )
     ];
   };
 
